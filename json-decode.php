b7ef226e099ecda037d6c85cf0da90c02cd10025
Falconguy <mr.digital.matt@gmail.com>
2019-01-15 14:38:53 +0800

    add

---
+++ json-decode.php
@@ -0,0 +1,15 @@
+<?php
+
+function w3rfunction($value,$key){
+    
+}
+
+$json = '{
+"Title": "The Cuckoos Calling",
+"Author": "Robert Galbraith",
+"Detail": {
+"Publisher": "Little Brown"
+}
+}';
+$jl = json_decode($json,true);
+array_walk_recursive($jl,"w3rfunction");?>
